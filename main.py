889840c48798d2c32ec2179892e755405f87fc5b
mkdir-not-war <mkdir-not-war@users.noreply.github.com>
2020-06-04 19:27:17 -0500

    more physics

--- main.py
+++ main.py
@@ -1,5 +1,58 @@
 import pygame
 
+#constants
+TILE_WIDTH = 32
+FPS = 30
+
+# physics
+GROUND_FRIC = 1.0
+AIR_FRIC = 1.0
+GRAVITY_ACCEL = 10
+TIME_STEP = 1.0/FPS
+
+def sum_tuples(tuples):
+	resultx = 0
+	resulty = 0
+	for x, y in tuples:
+		resultx += x
+		resulty += y
+	return (resultx, resulty)
+
+def tuple_mult(tup, scalar):
+	resultx = tup[0] * scalar
+	resulty = tup[1] * scalar
+	return (resultx, resulty)
+
+class Rect:
+	def __init__(self, pos, dim):
+		self.pos = pos
+		self.x = pos[0]
+		self.y = pos[1]
+		self.dim = dim
+		self.width = dim[0]
+		self.height = dim[1]
+
+	def move(self, dp):
+		self.x += dp[0]
+		self.y += dp[1]
+		return self
+
+	def get_center(self):
+		result = ((self.x + self.width)/2.0, (self.y + self.height)/2.0)
+		return result
+
+	def contains_point(self, point):
+		result = (
+			point >= self.x and
+			point < self.x+self.width and
+			point >= self.y and
+			point < self.y+self.height
+		)
+		return result
+
+	def collides_rect(self, rect):
+		sumrect = ()
+
 grey = pygame.Color(200, 200, 200)
 lightgrey = pygame.Color(125, 125, 125)
 darkred = pygame.Color(80, 0, 0)
@@ -31,15 +84,37 @@ magic_combos['neutral']['wind'] = 'refresh jump'
 magic_combos['wind']['neutral'] = 'tornado'
 magic_combos['wind']['wind'] = 'air pistol'
 
-# physics
-GROUND_FRIC = 1.0
-AIR_FRIC = 1.0
-GRAVITY_ACCEL = (0, 10)
-
 def update_physicsbodies(physicsbodies):
 	# get all new rects
-	# mark rects that collide with geometry
+	length = 0
+	new_rects = []
+	for pb in physicsbodies:
+		newrect = pb.rect
+		length += 1
+
+		# add all forces
+		sum_forces = sum_tuples(pb.forces)
+		# divide out mass and apply gravity
+		ddp = tuple_mult((sum_forces[0]/pb.mass, sum_forces[1]/pb.mass + GRAVITY_ACCEL), TIME_STEP/2.0)
+
+		# apply to velocity
+		pb.dp = sum_tuples([pb.dp, ddp])
+
+		# get delta position and apply with move()
+		newrect.move(tuple_mult(pb.dp, TIME_STEP))
+
+		# put it in the list
+		new_rects.append(newrect)
+
+	# if rect collides with geometry, clamp to nearest tile boundary
+
+	# if rect collides with other physics bodies and is "solid", mark
+
+	# if rect is collides with an attack, mark
+
 	# resolve rects
+	for pbi in range(length):
+		physicsbodies[pbi].rect = new_rects[pbi]
 
 class PhysicsBody:
 	def __init__(self, pos=(0, 0), width=TILE_WIDTH, height=TILE_WIDTH, mass=1.0):
@@ -48,11 +123,18 @@ class PhysicsBody:
 		self.dp = (0, 0)
 		self.forces = []
 
+	def get_pos(self):
+		result = (self.rect.x, self.rect.y)
+		return result
+
+	def get_dim(self):
+		result = (self.rect.width, self.rect.height)
+		return result
+
 class Player:
 	def __init__(self):
 		# physics stuff
-		physicsbody = PhysicsBody()
-
+		self.physicsbody = PhysicsBody()
 
 		# magic stuff
 		self.max_mana = 6
@@ -66,6 +148,10 @@ class Player:
 		self.max_spells_saved = self.max_mana
 		self.spells_used = [] 
 		self.spells_used_len = 0
+
+	def get_pos(self):
+		result = self.physicsbody.get_pos()
+		return result
 
 def player_update(player):
 	# thirty frames per second => 33 ms per frame
@@ -102,8 +188,6 @@ def use_element(player, e):
 		
 	else:
 		return 'out of mana'
-
-TILE_WIDTH = 32
 
 class MapData:
 	def __init__(self, dim=(0, 0)):
@@ -160,7 +244,6 @@ def main():
 
 	done = False
 	clock = pygame.time.Clock()
-	FPS = 30
 
 	# input stuff
 	pygame.joystick.init()
@@ -181,6 +264,9 @@ def main():
 	geometry = MapData()
 	geometry.load('map1')
 
+	# physics
+	physicsbodies = [player.physicsbody]
+
 	while not done:
 		clock.tick(FPS)
 
@@ -199,12 +285,14 @@ def main():
 				if event.key in curr_input:
 					curr_input.remove(event.key)
 
+		debug_func = player.get_pos
+
 		# keypad handle input
 		moveinput_dir = [0, 0]
 		if pygame.K_ESCAPE in curr_input:
 			done = True
-		if pygame.K_SPACE in curr_input:
-			pass
+		if pygame.K_SPACE in curr_input and len(prev_input) == 0:
+			output.append(debug_func())
 
 		if pygame.K_q in curr_input and len(prev_input) == 0:
 			output.append(use_element(player, equipped_element_Q))
@@ -213,8 +301,10 @@ def main():
 		elif pygame.K_e in curr_input and len(prev_input) == 0:
 			output.append(use_element(player, equipped_element_E))
 
-		# update mana and shit
+		# updates
 		output.append(player_update(player))
+
+		update_physicsbodies(physicsbodies)
 
 		# start drawing
 		screen.fill(grey)
